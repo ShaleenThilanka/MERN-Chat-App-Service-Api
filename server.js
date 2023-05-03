0b0ae4817ac1229a3298d7da340ad61ec01da7cb
ShaleenThilanka <shaleen.diss@gmail.com>
2023-05-03 21:31:43 +0530

    commit 1

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const http = require("http");
+const chats = require("./data/data");
+const dotenv = require("dotenv")
 const app = express();
+dotenv.config();
 const port = process.env.PORT || 5000
 const server = http.createServer(app);
 
 app.get("/",(req, res)=>{
 res.send("Api was running");
+})
 
+app.get("/api/chat",(req, res)=>{
+    res.send(chats);
 })
 
 app.listen(port,console.log("Server started on "+port));
