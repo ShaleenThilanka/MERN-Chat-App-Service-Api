30a1035e6742b14427d7f2696483806c8c2c0c1f
ShaleenThilanka <shaleen.diss@gmail.com>
2023-05-05 11:29:18 +0530

    done group remove

--- routes/chatRoutes.js
+++ routes/chatRoutes.js
@@ -14,8 +14,8 @@ const router = express.Router();
 router.route("/").post(protect, accessChat);
 router.route("/").get(protect, fetchChats);
 router.route("/group").post(protect, createGroupChat);
-// router.route("/rename").put(protect, renameGroup);
-// router.route("/group-remove").put(protect, removeFromGroup);
-// router.route("/group-add").put(protect, addToGroup);
+router.route("/rename").put(protect, renameGroup);
+router.route("/group-remove").put(protect, removeFromGroup);
+router.route("/group-add").put(protect, addToGroup);
 
 module.exports = router;
