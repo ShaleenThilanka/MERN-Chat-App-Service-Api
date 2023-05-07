04e48f7bb0ab8eafbe9df134570a6cb090086484
ShaleenThilanka <shaleen.diss@gmail.com>
2023-05-07 16:12:52 +0530

    changed

--- controller/messageController.js
+++ controller/messageController.js
@@ -6,7 +6,7 @@ const Chat = require("../models/chatModel");
 const allMessages = asyncHandler(async (req, res) => {
     try {
         const messages = await Message.find({ chat: req.params.chatId })
-            .populate("sender", "name pic email")
+            .populate("sender", "name pics email")
             .populate("chat");
         res.json(messages);
     } catch (error) {
@@ -32,7 +32,7 @@ const sendMessage = asyncHandler(async (req, res) => {
     try {
         var message = await Message.create(newMessage);
 
-        message = await message.populate("sender", "name pic");
+        message = await message.populate("sender", "name pics");
         message = await message.populate("chat");
         message = await User.populate(message, {
             path: "chat.users",
